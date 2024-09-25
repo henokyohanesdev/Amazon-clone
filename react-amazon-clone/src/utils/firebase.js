780bdfc934eeec097af6d50910237469f2570132
HenokYohanes <henokyohanesdev@gmail.com>
2024-09-25 14:39:29 -0500

    firebase authentication configured.

---
+++ react-amazon-clone/src/utils/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import 'firebase/compact/firestore'
+import 'firebase/compact/auth'
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAlxzNMp1afpDLBtgjsQz7HRzvO3Ep6T78",
+  authDomain: "clone-2d191.firebaseapp.com",
+  projectId: "clone-2d191",
+  storageBucket: "clone-2d191.appspot.com",
+  messagingSenderId: "463944623801",
+  appId: "1:463944623801:web:37d8bb5d946b3115411d68",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app)
